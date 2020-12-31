cc8206928b12654f1bea505e48f1a0e3526bf58d
zv1234 <2501942347@qq.com>
2020-12-31 18:26:44 +0800

    二分查找解题

--- mid_search/__init__.py
+++ mid_search/__init__.py
@@ -20,3 +20,44 @@ def mind(nums,target):
     return -1
 
 
+
+#69求开方 给定一个非负整数，求它的开方，向下取整。
+'''
+1.二分查找
+'''
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x==0:
+            return 0
+        l,r,ans=0,x,-1
+        while l<=r:
+            mid=l+(r-l)/2
+            if mid*mid<=x:
+                ans=mid
+                l=mid+1
+            else:
+                r=mid-1
+        return ans
+
+#34给定一个增序的整数数组和一个值，查找该值第一次和最后一次出现的位置。
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if nums==[]:
+            return [-1,-1]
+        l=0
+        r=len(nums)-1
+        while l<r:
+            pass
+
+
+
+
