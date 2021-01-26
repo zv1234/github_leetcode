58efc2ffd0d2b0986ba3e3194c37e2acdce5ac89
zv1234 <2501942347@qq.com>
2021-01-26 23:23:35 +0800

    链表基础

---
+++ 九章算法/链表/__init__.py
@@ -0,0 +1,17 @@
+#encoding:utf-8
+
+#当发现头节点会发生改变时候，创建一个头节点，当返回还是head时候不需要生成头节点
+
+# introduce dummy node是否添加头节点
+
+# 给定一个排序链表，删除所有重复的元素只留下原链表中没有重复的元素。
+
+
+
+#basic linked list skills
+
+
+
+
+
+# two pointers in linked list
