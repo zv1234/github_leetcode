e04e0a05b2c458dd6819c287a85003a95f699bf7
zv1234 <2501942347@qq.com>
2021-03-21 17:53:12 +0800

    二叉树问题

---
+++ Binary Tree & Divide Conquer/二叉树遍历.py
@@ -0,0 +1,476 @@
+#encoding:utf-8
+
+'''
+
+[二叉树的前序遍历](  # 二叉树的前序遍历)
+[二叉树的中序遍历](  # 二叉树的中序遍历)
+[二叉树的后序遍历](  # 二叉树的后序遍历)
+
+'''
+"""
+Definition of TreeNode:
+"""
+from collections import deque
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        # if root is None:
+        #     return []
+        # # 根左右
+        # return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        if root is None:
+            return []
+        stack = [root]
+        preorder = []
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
+
+    #中序遍历http://www.jiuzhang.com/solutions/binary-tree-inorder-traversal/
+    def inorderTraversal(self, root):
+        # write your code here
+        # if root is None:
+        #     return []
+        # return self.inorderTraversal(root.left)+[root]+self.inorderTraversal(root.right)
+        dummy=TreeNode(0)
+        dummy.right=root
+        stack=[]
+        stack.append(dummy)
+        inorder=[]
+        while stack:
+            node=stack.pop()
+            if node.right:
+                node=node.right
+                while node:
+                    stack.append(node)
+                    node=node.left
+            if stack:
+                inorder.append(stack[-1].val)
+        return inorder
+
+    def inorderTraversal(self, root):
+        if root == None:
+            return []
+        stack = []
+        output = []
+        node = root
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            output.append(node.val)
+
+            node = node.right
+        return output
+
+    def postorderTraversal(self, root):
+        # write your code here
+        # if root is None:
+        #      return []
+        # return self.postorderTraversal(root.left)+self.postorderTraversal(root.right)+[root.val]
+        stack = []
+        result = []
+        curnode = root
+        while stack or curnode:
+            while curnode:
+                stack.append(curnode)
+                curnode = curnode.left if curnode.left else curnode.right
+
+            curnode = stack.pop()
+            result.append(curnode.val)
+
+            if stack and stack[-1].left == curnode:
+                curnode = stack[-1].right
+            else:
+                curnode = None
+        return result
+    #二叉树层序遍历
+    def levelOrder(self, root):
+        if root is None:
+            return []
+        queue = deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
+
+
+# 97Maximum Depth of Binary Tree
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    def maxDepth(self, root):
+        # write your code here
+        if root is None:
+            return 0
+        left=self.maxDepth(root.left)
+        right=self.maxDepth(root.right)
+        return max(left,right)+1
+
+# 93. 平衡二叉树
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if this Binary tree is Balanced, or false.
+    """
+
+    def isBalanced(self, root):
+        balanced, _ = self.validate(root)
+        return balanced
+
+    def validate(self, root):
+        if root is None:
+            return True, 0
+        balanced, leftHeight = self.validate(root.left)
+        if not balanced:
+            return False, 0
+        balanced, rightHeight = self.validate(root.right)
+        if not balanced:
+            return False, 0
+
+        return abs(leftHeight - rightHeight) <= 1, max(leftHeight, rightHeight) + 1
+
+
+# 94. 二叉树中的最大路径和
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    import sys
+    def maxPathSum(self, root):
+        maxSum, _ = self.maxPathHelper(root)
+        return maxSum
+
+    def maxPathHelper(self, root):
+        if root is None:
+            return -sys.maxint, 0
+
+        left = self.maxPathHelper(root.left)
+        right = self.maxPathHelper(root.right)
+        maxpath = max(left[0], right[0], root.val + left[1] + right[1])
+        single = max(left[1] + root.val, right[1] + root.val, 0)
+
+        return maxpath, single
+
+
+# 70. 二叉树的层次遍历 II
+from collections import deque
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def levelOrderBottom(self, root):
+        # write your code here
+        if root is None :
+            return []
+        stack=deque([root])
+        result=[]
+        while stack:
+            lever=[]
+            for _ in range(len(stack)):
+                node=stack.popleft()
+                lever.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+            result.append(lever)
+        result.reverse()
+        return result
+
+# 71. 二叉树的锯齿形层次遍历
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+from collections import deque
+
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: A list of lists of integer include the zigzag level order traversal of its nodes' values.
+    """
+
+    def zigzagLevelOrder(self, root):
+        #注释的方法有错误不能100%通过
+        # write your code here
+        # length=1
+        # if root is None:
+        #     return []
+        # stack=deque([root])
+        # result=[]
+        # while len(stack) is not 0:
+        #     lever=[]
+        #     for _ in range(len(stack)):
+        #         node=stack.popleft()
+        #         lever.append(node.val)
+        #         if length%2==0:
+        #             if node.left:
+        #                 stack.append(node.left)
+        #             if node.right:
+        #                 stack.append(node.right)
+        #         else:
+        #             if node.right:
+        #                 stack.append(node.right)
+        #             if node.left:
+        #                 stack.append(node.left)
+        #     length+=1
+        #     result.append(lever)
+        # return result
+        import collections
+        # write your code here
+        if root is None:
+            return []
+        ans = []
+        q = collections.deque()
+        q.append(root)
+        # 正反向标志
+        isForward = 1
+        while len(q) is not 0:
+            row = []
+            for i in range(len(q)):
+                if q[0].left:
+                    q.append(q[0].left)
+                if q[0].right:
+                    q.append(q[0].right)
+                row.append(q[0].val)
+                q.popleft()
+            # 根据标志来确认当前层遍历的方向
+            row = row[::isForward]  # 翻转
+            ans += [row]
+            # 方向反转
+            isForward *= -1
+        return ans
+
+
+# 95. 验证二叉查找树
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    #中序遍历
+    def isValidBST(self, root):
+        self.lastVal = None
+        self.isBST = True
+        self.validate(root)
+        return self.isBST
+
+    def validate(self, root):
+        if root is None:
+            return
+        self.validate(root.left)
+        if self.lastVal is not None and self.lastVal >= root.val:
+            self.isBST = False
+            return
+        self.lastVal = root.val
+        self.validate(root.right)
+
+# 86. 二叉查找树迭代器
+"""
+Example of iterate a tree:
+iterator = BSTIterator(root)
+while iterator.hasNext():
+    node = iterator.next()
+    do something for node 
+"""
+
+class BSTIterator:
+    """
+    @param: root: The root of binary tree.
+    """
+    def __init__(self, root):
+        # do intialization if necessary
+        self.stack = []
+        while root != None:
+            self.stack.append(root)
+            root = root.left
+    """
+    @return: True if there has next node, or false
+    """
+    def hasNext(self):
+        # write your code here
+        return len(self.stack) > 0
+    """
+    @return: return next node
+    """
+    def _next(self):
+        # write your code here
+        node = self.stack[-1]
+        if node.right is not None:
+            n = node.right
+            while n != None:
+                self.stack.append(n)
+                n = n.left
+        else:
+            n = self.stack.pop()
+            while self.stack and self.stack[-1].right == n:
+                n = self.stack.pop()
+        return node
+
+
+# 11. 二叉查找树中搜索区间
+class Solution:
+    """
+    @param root: param root: The root of the binary search tree
+    @param k1: An integer
+    @param k2: An integer
+    @return: return: Return all keys that k1<=key<=k2 in ascending order
+    """
+    def searchRange(self, root, k1, k2):
+        # write your code here
+        if root is None:
+            return []
+        stack=[]
+        result=[]
+        node=root
+        while node or stack:
+            while node:
+                stack.append(node)
+                node=node.left
+            node=stack.pop()
+            if k1<=node.val<=k2:
+                result.append(node.val)
+            node=node.right
+        return result
+
+#85. 在二叉查找树中插入节点
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+
+    def insertNode1(self, root, node):
+        # write your code here
+        if root is None:
+            root = node
+            return root
+
+        if node.val < root.val:
+            root.left = self.insertNode1(root.left, node)
+        else:
+            root.right = self.insertNode1(root.left, node)
+
+        return root
+
+    def insertNode(self, root, node):
+        # write your code here
+        if root is None:
+            root=node
+            return root
+        t=root
+        while t:
+            if node.val<t.val:
+                if t.left is None:
+                    t.left=node
+                    return root
+                else:
+                    t=t.left
+                    continue
+            else:
+                if t.right is None:
+                    t.right=node
+                    return root
+                else:
+                    t=t.right
+                    continue
+
+
+# 87. 删除二叉查找树的节点
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: value: Remove the node with given value.
+    @return: The root of the binary search tree after removal.
+    """
+    #没看懂
+    def removeNode(self, root, value):
+        # write your code here
+        # if root is None:
+        #     return
+        # node=root
+        # while node:
+        #     if node.val>value
+        #         pre_node=node
+        #         node=node.left
+        #         continue
+        #     elif node.val<value:
+        #         pre_node=node
+        #         node=node.right
+        #         continue
+        #     else:
+        #         if node.left is None and node.right is not None:
+        #             pre_node.left=node.right
+        #         elif node.right is None and node.left is not None:
+        #             node=node.left
+        #         elif node.left is None and node.left is None:
+        #             node=None
+        #         else:
+        #             pre_node.left=node.left
+        # return root
+        if root is None:
+            return None
+        if root.val == value:
+            if root.left is None:
+                if root.right is None: #无子节点，直接删除
+                    return None
+                else:
+                    root = root.right #只有右儿子节点，用右儿子替换
+            elif root.right is None:
+                root = root.left #只有左儿子节点，用左儿子替换
+            else:
+                buffer = None #buffer用于存放要替换节点的父节点
+                tmp = root.right
+                while tmp.left is not None:
+                    buffer = tmp
+                    tmp = tmp.left
+                root.val, tmp.val = tmp.val, root.val
+                if buffer is None: #buffer为空，则root的右儿子即是目标替换节点
+                    root.right = self.removeNode(tmp, value)
+                else:
+                    buffer.left = self.removeNode(tmp, value)
+        elif value < root.val:
+            root.left = self.removeNode(root.left, value)
+        else:
+            root.right = self.removeNode(root.right, value)
+        return root
+
+
+
+
